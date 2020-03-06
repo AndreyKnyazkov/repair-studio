0379ac6e1e1d8c737006a4c2c2eec798e6939dde
Andrey Knyazkov <martinadnrey1998@gmail.com>
2020-03-06 12:03:41 +0300

    Сверстан блок на js с раскрывающимся списком

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,14 @@
-var button = document.querySelector('#button');
-var modal = document.querySelector('#modal');
-var close = document.querySelector('#close')
-
-button.addEventListener('click', function() {
-  modal.classList.add('modal_active');
-});
-
-close.addEventListener('click', function() {
-  modal.classList.remove('modal_active');
-})
+var accItem = document.getElementsByClassName('questions-answer');
+var accHD = document.getElementsByClassName('questions-answer__title');
+for (i = 0; i < accHD.length; i++) {
+    accHD[i].addEventListener('click', toggleItem, false);
+}
+function toggleItem() {
+    var itemClass = this.parentNode.className;
+    for (i = 0; i < accItem.length; i++) {
+        accItem[i].className = 'questions-answer close';
+    }
+    if (itemClass == 'questions-answer close') {
+        this.parentNode.className = 'questions-answer open';
+    }
+}
