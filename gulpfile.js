f425fb68203f5df5787e445b6e1b0cb429e1fae3
Andrey Knyazkov <martinadnrey1998@gmail.com>
2020-03-08 22:25:33 +0300

    исправлен баг с svg

--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,12 @@ gulp.task('move-js', function(done) {
   return gulp.src('./src/js/*.js')
   .pipe(gulp.dest('dist/js/'));
   done();
-}); 
+});
+gulp.task('move-svg', function (done) {
+  return gulp.src('./src/img/*.svg')
+    .pipe(gulp.dest('dist/img/'));
+  done();
+});
 
 gulp.task('htmlmin', function(done) {
   return gulp.src('src/*.html')
@@ -31,7 +36,7 @@ gulp.task('fonts', function(done) {
 }); 
 
 gulp.task('tinyPNG', function (done) {
-  return gulp.src('./src/img/**/*.{png,jpg,jpeg}')
+  return gulp.src('./src/img/**/*.{png,jpg,jpeg,svg}')
       .pipe(tinyPNG({
           key: '8CwslrdFH37Xwp9p0LrLrJTN3NnDL94b',
       }))
@@ -39,7 +44,7 @@ gulp.task('tinyPNG', function (done) {
       done();
 });
 
-gulp.task('default', gulp.parallel('minify-css', 'move-js', 'htmlmin', 'fonts', 'tinyPNG', function (done){
+gulp.task('default', gulp.parallel('minify-css', 'move-js', 'htmlmin', 'fonts', 'tinyPNG', 'move-svg', function (done){
   done();
 }));
 
