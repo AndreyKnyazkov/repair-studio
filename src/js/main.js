c129472a8341bc0dac3094cbea28767270c3472f
Andrey Knyazkov <martinadnrey1998@gmail.com>
2020-03-08 19:38:43 +0300

    Верстка всего макета закончена кроме подбора текста и картинок к динамическим элементам

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,8 @@
 var accItem = document.getElementsByClassName('questions-answer');
 var accHD = document.getElementsByClassName('questions-answer__title');
+var popupName = ('.popup-answer');
+var link = ('.button');
+
 for (i = 0; i < accHD.length; i++) {
     accHD[i].addEventListener('click', toggleItem, false);
 }
@@ -12,3 +15,29 @@ function toggleItem() {
         this.parentNode.className = 'questions-answer open';
     }
 }
+/* modal popup */
+$(document).ready(function($) {
+  $('.button').on('click', function() {
+    $('.modal-answer').text($(this).attr('data-popup'));
+    $('.modal').css("display", "flex").hide().fadeIn();
+    return false;
+  });	
+	
+  $('.modal-close').click(function() {
+    $(this).parents('.modal').fadeOut();
+    return false;
+  });		
+ 
+  $(document).keydown(function(e) {
+    if (e.keyCode === 27) {
+      e.stopPropagation();
+      $('.modal').fadeOut();
+    }
+  });
+	
+  $('.modal').click(function(e) {
+    if ($(e.target).closest('.modal-block').length == 0) {
+      $(this).fadeOut();					
+    }
+  });
+});
