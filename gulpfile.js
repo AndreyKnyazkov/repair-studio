9ae7be2258b506550cf4e88c10025f4ec2f84ceb
Andrey Knyazkov <martinadnrey1998@gmail.com>
2020-03-09 12:42:40 +0300

    Обновлен gulpfile, исправлены ошибки из поста на стене ВКонтакте

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
-var gulp = require('gulp');
-var cleanCSS = require('gulp-clean-css');
-var htmlmin = require('gulp-htmlmin');
-var tinyPNG = require('gulp-tinypng-compress');
+const gulp = require('gulp');
+const cleanCSS = require('gulp-clean-css');
+const htmlmin = require('gulp-htmlmin');
+// const tinyPNG = require('gulp-tinypng-compress');
+const tinify = require('gulp-tinify');
 
 
 gulp.task('minify-css', function(done) {
@@ -11,16 +12,23 @@ gulp.task('minify-css', function(done) {
   done();
 }); 
 
+gulp.task('imagemin', () => {
+  return gulp.src('src/img/**/*.*')
+    .pipe(tinify('8CwslrdFH37Xwp9p0LrLrJTN3NnDL94b'))
+    .pipe(gulp.dest('dist/img/'))
+});
+
 gulp.task('move-js', function(done) {
   return gulp.src('./src/js/*.js')
   .pipe(gulp.dest('dist/js/'));
   done();
 });
-gulp.task('move-svg', function (done) {
-  return gulp.src('./src/img/**/*.svg')
-    .pipe(gulp.dest('dist/img/'));
-  done();
-});
+
+// gulp.task('move-svg', function (done) {
+//   return gulp.src('./src/img/**/*.svg')
+//     .pipe(gulp.dest('dist/img/'));
+//   done();
+// });
 
 gulp.task('htmlmin', function(done) {
   return gulp.src('src/*.html')
@@ -35,19 +43,24 @@ gulp.task('fonts', function(done) {
   done();
 }); 
 
-gulp.task('tinyPNG', function (done) {
-  return gulp.src('./src/img/**/*.{png,jpg,jpeg}', {
-    dot: true,
-    ignore: '/**/*.svg'
-  })
-      .pipe(tinyPNG({
-          key: '8CwslrdFH37Xwp9p0LrLrJTN3NnDL94b',
-      }))
-      .pipe(gulp.dest('dist/img/'));
-      done();
-});
+// gulp.task('tinyPNG', function (done) {
+//   return gulp.src('./src/img/**/*.{png,jpg,jpeg}', {
+//     dot: true,
+//     ignore: '/**/*.svg'
+//   })
+//       .pipe(tinyPNG({
+//           key: '8CwslrdFH37Xwp9p0LrLrJTN3NnDL94b',
+//       }))
+//       .pipe(gulp.dest('dist/img/'));
+//       done();
+// });
 
-gulp.task('default', gulp.parallel('minify-css', 'move-js', 'htmlmin', 'fonts', 'tinyPNG', 'move-svg', function (done){
+/* Включить сжатие изображений */
+/* gulp.task('default', gulp.parallel('minify-css', 'move-js', 'htmlmin', 'fonts', 'imagemin', function (done){
+  done();
+})); */
+/* Отключить сжатие изображений */
+gulp.task('default', gulp.parallel('minify-css', 'move-js', 'htmlmin', 'fonts', function (done) {
   done();
 }));
 
